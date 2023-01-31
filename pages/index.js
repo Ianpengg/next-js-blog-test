39fc95b1bd3c4e4ff2801823e48c575730858270
chenyi <ian01050.ee10@nycu.edu.tw>
2023-01-31 20:35:21 +0800

    [Feat] Add Home text

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,14 @@ import Link from 'next/link'
 
 export default function Home({ }) {
   return (
-    <div className="h-screen w-full text-center">
-      <div className="mxa-w-[1240px] w-full h-full">
-        
+    <div>
+      <div
+        className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row
+    max-w-full justify-evenly mx-auto items-center z-0"
+      >
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+          Home
+        </h3>
       </div>
     </div>
   )
